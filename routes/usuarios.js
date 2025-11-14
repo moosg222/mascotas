d7c1518779b5b53e9881a3bba0bd9006ba0c62cd
moosg222 <moosg222@gmail.com>
2025-11-14 00:39:08 -0500

    ponerle .bind a todas las rutas

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -4,13 +4,13 @@ import UsuariosController from "../controllers/usuarios.js";
 import { verificarToken } from "../helpers/autenticacion.js";
 
 route.post("/register", UsuariosController.register.bind(UsuariosController));
-route.post("/login", UsuariosController.login);
-route.get("/perfil", verificarToken, UsuariosController.profile);
-route.put("/perfil", verificarToken, UsuariosController.actualizarPerfil);
+route.post("/login", UsuariosController.login.bind(UsuariosController));
+route.get("/perfil", verificarToken, UsuariosController.profile.bind(UsuariosController));
+route.put("/perfil", verificarToken, UsuariosController.actualizarPerfil.bind(UsuariosController));
 route.delete("/perfil", verificarToken, UsuariosController.deleteProfile.bind(UsuariosController));
-route.post("/recuperar", UsuariosController.solicitarRestablecimiento);
-route.post("/reset-password", UsuariosController.resetPassword);
-route.get("/verificarEmail", UsuariosController.verificarEmail);
-route.post("/reenviarVerificacion", UsuariosController.reenviarVerificacion);
+route.post("/recuperar", UsuariosController.solicitarRestablecimiento.bind(UsuariosController));
+route.post("/reset-password", UsuariosController.resetPassword.bind(UsuariosController));
+route.get("/verificarEmail", UsuariosController.verificarEmail.bind(UsuariosController));
+route.post("/reenviarVerificacion", UsuariosController.reenviarVerificacion.bind(UsuariosController));
 
 export default route;
