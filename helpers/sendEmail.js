8d17a8ade4ade27274c53d90226bc6733ae810c3
moosg222 <moosg222@gmail.com>
2025-11-13 23:55:57 -0500

    ajuste en sendEmail2

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,17 +1,17 @@
 import { Resend } from "resend";
 import "dotenv/config";
 
-
-
 const resend = new Resend(process.env.RESEND_API_KEY);
-
-console.log("RESEND_API_KEY:", process.env.RESEND_API_KEY ? "CARGADA" : "VACÍA");
 
 export default async function sendEmail(to, subject, htmlContent) {
   try {
+    console.log(
+      "RESEND_API_KEY:",
+      process.env.RESEND_API_KEY ? "CARGADA" : "VACÍA"
+    );
     const result = await resend.emails.send({
-      from: "<no-reply@resend.dev>",
-      to:[to,"moosg222@gmail.com"] ,
+      from: "Acme <onboarding@resend.dev>",
+      to: [to, "moosg222@gmail.com"],
       subject,
       html: htmlContent,
     });
